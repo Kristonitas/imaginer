26e36dc9b97a8100e834e872190cf4c82e11124d
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2019-12-31 14:31:38 +0200

    Render current date/time

--- src/renderer.ts
+++ src/renderer.ts
@@ -6,24 +6,45 @@ type Options = {
 	background?: string;
 };
 
+const now = () => {
+	const now = new Date();
+	let dd = "" + now.getDate();
+	let mm = "" + (now.getMonth() + 1);
+	const yyyy = now.getFullYear();
+	if (dd.length < 2) {
+		dd = "0" + dd;
+	}
+
+	if (mm.length < 2) {
+		mm = "0" + mm;
+	}
+
+	const time =
+		now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+
+	return `${yyyy}-${mm}-${dd}\n${time}`;
+};
+
 const render = (width: number, height: number, options: Options) => {
 	const { dev, scale = 1, background = "white" } = options;
 	const canvas = createCanvas(width * scale, height * scale);
 	const ctx = canvas.getContext("2d");
 	ctx.scale(scale, scale);
+	ctx.font = "12px Sans-Serif";
 
 	ctx.fillStyle = background;
 	ctx.fillRect(0, 0, width, height);
 
 	if (dev) {
 		ctx.fillStyle = "black";
-		ctx.font = "12px Sans-Serif";
 		ctx.fillText("dev", 4, height - 4);
 	}
 
 	ctx.fillStyle = "black";
-	ctx.font = "12px Sans-Serif";
 	ctx.fillText(`${width}x${height}`, 4, 16);
+
+	ctx.textAlign = "center";
+	ctx.fillText(now(), width / 2, height / 2);
 
 	const stream = canvas.createJPEGStream();
 	return stream;
